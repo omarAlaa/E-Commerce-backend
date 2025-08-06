86fad5d68bbc9eabf1535434b4d878c06541fc98
omarAlaa <omarmuhamed10@gmail.com>
2025-08-07 02:52:15 +0300

    CORS updated

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ const category = require('./models/category')
 require('dotenv').config()
 
 app.use(express.json())
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://e-commerce-frontend.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}))
+app.use(cors())
 
 
 const PORT = process.env.PORT
