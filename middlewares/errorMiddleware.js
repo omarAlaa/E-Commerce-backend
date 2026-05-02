5661b84af91952c09cd42bec9c08e1e38d51fdd1
omarAlaa <omarmuhamed10@gmail.com>
2026-05-02 23:28:14 +0300

    remove error logs

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,14 +1,10 @@
 const errorMiddleware = (error, req, res, next) => {
-    console.log(error)
-    
     if (error.code === 11000) {
         const field = Object.keys(error.keyValue)[0]
         const message = `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`
 
         return res.status(409).json({ message })
     }
-
-    console.log(error.message)
 
     return res.status(500).json({ message: 'Error occured, please try again later' })
 }
